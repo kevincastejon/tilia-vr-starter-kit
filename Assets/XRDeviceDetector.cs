78193c388e139defb372cb3383a33db11b39092c
Kevin Castejon <contact@kevincastejon.fr>
2020-10-29 22:23:19 +0100

    Initial commit

---
+++ Assets/XRDeviceDetector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRDeviceDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRDevice.deviceLoaded += OnDeviceLoaded;
+    }
+
+    private void OnDeviceLoaded(string obj)
+    {
+        GameSettings.XR_DEVICE_LOADED = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
