96712a8d307f19ba75152aba36aba326776b72f8
Kevin Castejon <kevincastejon@hotmail.fr>
2020-11-08 17:42:23 +0100

    updated oculus integration package version

---
+++ Assets/Scripts/LightVariator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+public class LightVariator : MonoBehaviour
+{
+    public void ChangeLight(Single amount)
+    {
+        Debug.Log(1.25f * amount);
+        RenderSettings.skybox.SetFloat("_Exposure", 1.25f*amount);
+    }
+}
